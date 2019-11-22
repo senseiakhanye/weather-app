313ebbd4d36af8d4e27fa5438a02ba89ae2a378a
Katleho <akhanye@student.fr.42>
2019-11-22 04:34:10 +0200

    updates to weather app and setup node server

--- api/server.js
+++ api/server.js
@@ -1,16 +1,24 @@
+const weatherService = require('../app');
 const express = require("express");
-const path = require('path');
-const weatherservice = require("../utils/geocode");
+const geocode = require('../utils/geocode');
 
-const folderLocation = path.join(__dirname, "../html");
 const app = express();
-
-app.use(express.static(folderLocation));
+console.log(weatherService);
+console.log(geocode);
 
 app.get('/api/weather', (req, res) => {
-    res.send(__dirname);
+    if (req.query.address === undefined) {
+        return res.send({ error: "No address provided"});
+    }
+    console.log(weatherService);
+    weatherService(req.query.address, (data) => {
+        res.send( {
+            forecast: data,
+            locaton: req.query.address
+        })
+    })
 });
 
-app.listen(3005, () => {
-    console.log("Listnening at port 3005");
+app.listen(4005, () => {
+    console.log("Listnening at port 4005");
 })
--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const weatherservice = require('./utils/geocode');
 
-if (process.argv.length < 3) {
-    return console.log("No arguments found");
+// if (process.argv.length < 3) {
+//     return console.log("No arguments found");
+// }
+
+// const areaName = process.argv[2];
+
+const getWeather = (address, callback) => {
+    console.log(address);
+    weatherservice.geocode(address, (error, data) => {
+        if (error !== undefined) {
+            if (callback !== undefined) {
+                return callback(error);
+            }
+        }
+        weatherservice.forecast(data, (error, data) => {
+            if (error !== undefined && callback !== undefined) {
+                return callback(error);
+            }
+            return callback(data);
+        })
+
+    })  
 }
 
-const areaName = process.argv[2];
-
-weatherservice.geocode(areaName, (error, data) => {
-    if (error !== undefined) {
-        // return console.log(error)
-    }
-    weatherservice.forecast(data, (error, data) => {
-        if (error !== undefined) {
-            // return console.log(error);
-        }
-        // console.log(data);
-    })
-})
+module.exports = getWeather;
